703a2343736c8fcbe14e236ba2f88344815b58fc
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-03-03 00:46:30 -0500

    someFixes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,105 +1,122 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faWordpress } from "@fortawesome/free-brands-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 const Cards = (props) => {
   return (
     <StyledCard>
-  
-    <div className='card'>
-        <div className='card-body'>
-            <img src={props.img}></img>
-            <h2 className='card-title'>{props.title}</h2>
-            <p className='card-description'>{props.description}</p>
-
+      <div className="card">
+        <div className="card-body">
+          <img src={props.img}></img>
+          <h2 className="card-title">{props.title}</h2>
+          <p className="card-description">{props.description}</p>
         </div>
-        <a className='card-btn' target="_blank" rel="noopener noreferrer" href={props.url}>Ver Proyecto </a>
-    </div> 
- 
+        <div className="buttons">
+          <a
+            style={{ alignItems: "center" }}
+            className="card-btn"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={props.url}
+          >
+            Ver Proyecto
+            <FontAwesomeIcon icon={faGlobe} className="cardgit" />
+          </a>
+          <a
+            className="card-btn"
+            style={{ alignItems: "center" }}
+            href={props.git}
+            rel="noopener"
+            target="_blank"
+          >
+            Codigo
+            <FontAwesomeIcon icon={faGithub} className="cardgit" />
+          </a>
+        </div>
+      </div>
     </StyledCard>
-  )
-}
+  );
+};
 
 const StyledCard = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap");
 
-@import url("https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap");
-
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: Oxygen, sans-serif;
-  margin: 2rem;
-}
-a{
-  text-decoration:none;
-  display:flex;
-  justify-content:center;
-  align-content:center;
-}
-
-
-.card {
-  overflow: hidden;
-  width: 400px;
-  min-height: 430px;
-  margin: 10px;
-  box-shadow: 0 2px 20px gray;
-  border-radius:  0.2rem;
-  flex-direction: column;
-  justify-content: space-evenly;
-  cursor: pointer;
-  transition: transform 200ms ease-in;
-  
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
-.card:hover{
-  transform: scale(1.02);
 
-}
-.card img{
+  body {
+    font-family: Oxygen, sans-serif;
+    margin: 2rem;
+  }
+  a {
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+
+  .card {
+    overflow: hidden;
+    width: 400px;
+    min-height: 430px;
+    margin: 10px;
+    box-shadow: 0 2px 20px gray;
+    border-radius: 0.2rem;
+    flex-direction: column;
+    justify-content: space-evenly;
+    cursor: pointer;
+    transition: transform 200ms ease-in;
+  }
+  .card:hover {
+    transform: scale(1.02);
+  }
+  .card img {
     height: 12rem;
     width: 100%;
     object-fit: cover;
   }
 
- .card-title {
+  .card-title {
     padding: 1rem;
   }
 
- .card-description {
+  .card-description {
     padding: 0 1rem;
   }
 
-  .card-btn{
-    padding: 1rem;
+  .card-btn {
+    padding: 0.5rem;
     font-family: inherit;
     font-weight: bold;
     font-size: 1rem;
     margin: 1rem;
-    border: 2px solid ;
+    border: 2px solid;
     background: transparent;
     color: #2e5a94;
     border-radius: 0.2rem;
-    transition: background 200ms ease-in, color 200ms ease-in;
+    transition: 200ms ease-in, color 200ms ease-in;
+  }
 
- 
-}
-
-
-.card-btn:hover  {
-  
+  .buttons {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .cardgit {
+    font-size: 30px;
+    margin-left: 5px;
+  }
+  .card-btn:hover {
     transform: scale(1.02);
-    
-    background: #2e5a94;;
+
+    background: #2e5a94;
     color: white;
-}
+  }
+`;
 
-
-`
-
-
-export default Cards
+export default Cards;
--- src/pages/Acerca.jsx
+++ src/pages/Acerca.jsx
@@ -15,36 +15,42 @@ const Acerca = () => {
             title="Weather-APP"
             description="App que permite ve el clima de tu ciudad!"
             url="https://weather-p40ousxb7-josemavsg.vercel.app"
+            git="https://github.com/JosemAVSG/weather-app"
           />
           <Cards
             img={homeimg.bikes}
             title="Search bikes"
             description="App que permite buscar bicicletas robadas!"
             url="https://coding-challenge-frontend-react-6dcyxqexh-josemavsg.vercel.app/"
+            git="https://github.com/JosemAVSG/coding-challenge-frontend-react"
           />
           <Cards
             img={homeimg.rick}
             title="Rick and Morty Chars "
             description="App que permite ver los personajes de rick and morty"
             url="https://rickandmorty-hyjn00rgb-josemavsg.vercel.app/"
+            git="https://github.com/JosemAVSG/rickandmorty"
           />
           <Cards
             img={homeimg.pokemon}
             title="Pokemon y Mapa de mapbox"
             description="Integracion de mapa de mapbox y lista de pokemon"
             url="https://prueba-manuel.vercel.app/"
+            git="https://github.com/JosemAVSG/Prueba_manuel"
           />
           <Cards
             img={homeimg.inventario}
             title="Sistema de inventario"
             description="Proyecto Full Stack, Back: express, Front: React"
             url="https://sistema-de-inventario-client-production.up.railway.app/"
+            git="https://github.com/JosemAVSG/Sistema-de-inventario-Client"
           />
              <Cards
             img={homeimg.p2henry}
             title="Movie APP"
             description="Proyecto Full Stack, Bootcamp Soy Henry #2"
             url="https://peppy-mooncake-dcfe44.netlify.app/"
+            git="https://github.com/JosemAVSG/P2-henrybootcamp"
           />
         </div>
       </div>
