b295244b46ef15538dddb76179f38223219a676c
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-03-08 16:08:18 -0500

    fixing inventory project

--- src/pages/Acerca.jsx
+++ src/pages/Acerca.jsx
@@ -22,7 +22,7 @@ const Acerca = () => {
             img={homeimg.inventario}
             title="Sistema de inventario"
             description="Proyecto Full Stack, Back: express, Front: React"
-            url="https://sistema-de-inventario-client-production.up.railway.app/"
+            url="https://sistema-de-inventario-client-8pxwmxu4e-josemavsg.vercel.app/"
             git="https://github.com/JosemAVSG/Sistema-de-inventario-Client"
             tech={["React", "Tailwind", "HTML","CSS","Mongo"]}
           />
