93c5102141257773effb4aa303610e7cabae364b
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-04-07 15:52:09 -0500

    fixxing

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,12 @@ import Navigation from "./components/Nav";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer";
-import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <div>
       <Router>
         <Navigation></Navigation>
-         <Analytics/>
+       
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/proyectos" element={<Acerca />} />
